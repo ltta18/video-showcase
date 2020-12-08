9927eae60efafc45b9286b2dda15070b6bbbb7c8
ltta18 <thuylinh7720@gmail.com>
2020-12-08 15:01:47 +0700

    Customize Button before playing + Loading Spinner

--- src/pages/VideoPlayer/index.jsx
+++ src/pages/VideoPlayer/index.jsx
@@ -1,18 +1,20 @@
 import { Button, Card } from "antd";
-import { useRef, useState } from "react";
-import { Player } from 'video-react';
+import { useEffect, useRef, useState } from "react";
+import { Player, playerReducer } from 'video-react';
 import ControlBar from "video-react/lib/components/control-bar/ControlBar";
-import { CaretRightOutlined, PauseOutlined, ForwardOutlined, BackwardOutlined } from '@ant-design/icons';
+import { ArrowLeftOutlined, PauseOutlined, ForwardOutlined, BackwardOutlined } from '@ant-design/icons';
 import 'video-react/dist/video-react.css';
 import ReplayControl from "video-react/lib/components/control-bar/ReplayControl";
 import ForwardControl from "video-react/lib/components/control-bar/ForwardControl";
 import CurrentTimeDisplay from "video-react/lib/components/time-controls/CurrentTimeDisplay";
 import TimeDivider from "video-react/lib/components/time-controls/TimeDivider";
 import VolumeMenuButton from "video-react/lib/components/control-bar/VolumeMenuButton";
+import './index.css';
+import BigPlayButton from "video-react/lib/components/BigPlayButton";
+import LoadingSpinner from "video-react/lib/components/LoadingSpinner";
 
 const VideoPlayer = () => {
   const player = useRef();
-  const [isPlay, setIsPlay] = useState(true)
 
   const setMuted = muted => {
     player.muted = muted;
@@ -24,8 +26,7 @@ const VideoPlayer = () => {
   }
 
   const play = () => {
-    isPlay ? player.current.actions.pause() : player.current.actions.play();
-    setIsPlay(!isPlay);
+    player.current.actions.play();
   }
 
   const changeCurrentTime = seconds => {
@@ -42,24 +43,33 @@ const VideoPlayer = () => {
     player.current.video.video.volume = volume + steps;
   }
 
+  // useEffect(() => {
+  //   player && play();
+  // }, [])
+
   return (
+    <>
+      <ArrowLeftOutlined
+        className="back-arrow"
+      />
       <Player
-          autoPlay
-          playsInline
           ref={player}
           className="video-player"
-          height='100%'
           fluid={false}
+          autoPlay
+          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
         >
-          <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"/>
-          <ControlBar autoHide={true} autoPlay>
+          <LoadingSpinner />
+          <BigPlayButton position="center" />
+          <ControlBar autoHide={true}>
             <ReplayControl seconds={10} order={1.1} />
             <ForwardControl seconds={30} order={1.2} />
             <CurrentTimeDisplay order={4.1} />
             <TimeDivider order={4.2} />
             <VolumeMenuButton />
-        </ControlBar>
+          </ControlBar>
         </Player>
+    </>
   );
 }
 
